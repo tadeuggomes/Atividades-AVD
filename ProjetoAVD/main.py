d782d0ca2e257351d717e4939e4132ce8cf989f1
Matheus Melquiades <matheusmelqn@gmail.com>
2026-08-20 21:32:28 -0300

    Aula20-08

--- ProjetoAVD/main.py
+++ ProjetoAVD/main.py
@@ -1,63 +1,15 @@
 import requests
 
-url =  "https://servicodados.ibge.gov.br/api/v3/agregados/4093/periodos/201201|201202-202601/variaveis/4093|4096|4099|12466?localidades=N3[26]&classificacao=2[all]"
-
+url = "https://dados.recife.pe.gov.br/api/action/datastore_search?resource_id=d6f586c3-34c5-4414-9804-6fdfbd75c7db&limit=200000"
 
 r = requests.get(url)
-print ("Status Code:", r.status_code)
-
+print("Status Code:", r.status_code)
 
 data = r.json()
 
-print (data[0]['resultados'][1]['series'][0]['serie'])
+print(data['result']['records'][0])
 
+registros = data['result']['records']
 
-valores0 = data[0]['resultados'][0]['series'][0]['serie']
-
-with open("TOTAL4093.json", "w") as f:
-    f.write(str(valores0))
-
-valores1 = data[0]['resultados'][1]['series'][0]['serie']
-
-with open("homens4093.json", "w") as f:
-    f.write(str(valores1))
-
-valores2 = data[0]['resultados'][2]['series'][0]['serie']
-
-with open("mulheres4093.json", "w") as f:
-    f.write(str(valores2))
-
-valores3 = data[1]['resultados'][0]['series'][0]['serie']
-with open("TOTAL4096.json", "w") as f:
-    f.write(str(valores3))
-
-valores4 = data[1]['resultados'][1]['series'][0]['serie']
-with open("homens4096.json", "w") as f:
-    f.write(str(valores4))
-
-valores5 = data[1]['resultados'][2]['series'][0]['serie']
-with open("mulheres4096.json", "w") as f:
-    f.write(str(valores5))
-
-valores6 = data[2]['resultados'][0]['series'][0]['serie']
-with open("TOTAL4099.json", "w") as f:
-    f.write(str(valores6))
-
-valores7 = data[2]['resultados'][1]['series'][0]['serie']
-with open("homens4099.json", "w") as f:
-    f.write(str(valores7))
-
-valores8 = data[2]['resultados'][2]['series'][0]['serie']
-with open("mulheres4099.json", "w") as f:
-    f.write(str(valores8))
-
-valores9 = data[3]['resultados'][0]['series'][0]['serie']
-with open("TOTAL12466.json", "w") as f:
-    f.write(str(valores9))
-
-valores10 = data[3]['resultados'][1]['series'][0]['serie']
-with open("homens12466.json", "w") as f:
-    f.write(str(valores10))
-valores11 = data[3]['resultados'][2]['series'][0]['serie']
-with open("mulheres12466.json", "w") as f:
-    f.write(str(valores11))
+with open("chamados_samu.json", "w") as f:
+    f.write(str(registros))
